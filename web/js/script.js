efd13f7d796348aa89b95c9fdfc61d76bd677c68
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-12-29 17:53:09 +0530

    done

--- web/js/script.js
+++ web/js/script.js
@@ -11,3 +11,17 @@ categorymenuclosebtn.addEventListener("click",()=>{
     categorymenu.style.left="-350px";
      document.body.style.overflow="auto"; 
 })
+
+let openmenu =document.querySelector(".openmenu");
+let menu = document.querySelector(".menu")
+let menuclosebtn =document.querySelector(".menuclosebtn")
+
+
+openmenu.addEventListener("click",()=>{
+    menu.style.top="0";
+     document.body.style.overflow="hidden";
+})
+menuclosebtn.addEventListener("click",()=>{
+    menu.style.top="-350px";
+     document.body.style.overflow="auto";
+})
